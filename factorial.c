95c6e8bef7d962c9ba70ebe2aafa3382301a8b9c
anushkasingh242007-hue <anushkasingh242007@gmail.com>
2026-06-09 21:45:05 +0530

    Add files via upload

---
+++ factorial.c
@@ -0,0 +1,13 @@
+```c
+#include <stdio.h>
+int main()
+{
+    int n,f =1;
+    scanf(%d" , &n);
+        for (int i =1; i<=n; i++)
+        {
+            f = f*i;
+    }
+            printf("Factorail of %dis %d",n,f);
+            return 0;
+    }
---
+++ table.c
@@ -0,0 +1,16 @@
+```c
+#include <stdio.h>
+int main()
+{
+    int num ,i, tab;
+    printf("enter the number: ");
+    scanf(%d" , &num);
+        printf("/ntable of %d is :/n" ,num);
+        for(i=1; i<+10; i+=)
+        {
+        tab = num*i;
+        printf("%d *%d =%d/n" , num,i,tab);
+        }
+        getch();
+        return 0;
+    }
